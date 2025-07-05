dd0867206d324a36334ef0114054d305eca270ff
JitBanerjee03 <jitbanerjee294@gmail.com>
2025-07-05 11:31:27 +0530

    modifications

--- src/store/ContextProvider.jsx
+++ src/store/ContextProvider.jsx
@@ -15,22 +15,6 @@ export const ContextProvider = ({ children }) => {
     const [isloggedIn, setLoggedIn] = useState(false);
     const [reviewer, setReviewer] = useState({});
     const [journalReview, dispatchReview] = useReducer(journalReviewReducer, []);
-
-    // Cross-tab + cross-domain logout via postMessage
-    useEffect(() => {
-    const messageHandler = (event) => {
-        if (event.data?.type === 'LOGOUT') {
-        localStorage.removeItem('jwtToken');
-        console.log('Token cleared via postMessage logout');
-        window.location.href = 'https://journal-management-system-frontend.vercel.app/login';
-        }
-    };
-
-    window.addEventListener('message', messageHandler);
-    return () => {
-        window.removeEventListener('message', messageHandler);
-    };
-    }, []);
     
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
